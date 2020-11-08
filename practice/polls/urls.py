045699f8d010f43f32d200c0d64b9d7543a1c9bf
msd hrk <domorugan123@gmail.com>
2020-11-09 01:49:43 +0900

    First Commit

---
+++ practice/polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+app_name='polls' #名前空間設定
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('<int:pk>/', views.detail, name='detail'),
+#     path('<int:pk>/results/', views.results, name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
